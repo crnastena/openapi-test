ccc00ad479cd87801dd5f1bb3d02e77198dcbd72
Todor Rikic <trikic@icapitalnetwork.com>
2022-12-05 13:58:34 +0000

    oat-007 - force checkin

--- app/controllers/api/v1/capital/capital_controller.rb
+++ app/controllers/api/v1/capital/capital_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Api::V1::Capital::CapitalController < ApplicationController
   def index
-    render json: { message: "capital" }, status: :ok
+    render json: { message: "capital controller" }, status: :ok
   end
 end
